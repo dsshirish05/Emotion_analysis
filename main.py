312c8aee3c498fa606c584a2f08131c7b9374fe1
Shirish D S <23z148@psgitech.ac.in>
2025-05-19 18:48:40 +0530

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from wordcloud import WordCloud
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
